237c114b427fc58fd83ac87e930de07988a5c904
SuleymanovTahir <suleymanov.takhir@gmail.com>
2025-10-29 17:31:55 +0400

    добавил новые эндпоинты в main.py

--- backend/main.py
+++ backend/main.py
@@ -100,10 +100,11 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://mlediamant.com",
+        "http://mlediamant.com",
+        "http://91.201.215.32:8000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        "https://cdn.mlediamant.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
