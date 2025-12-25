2c23fc5035b35264593d59eae0bd054eeb471032
SuleymanovTahir <90999791+SuleymanovTahir@users.noreply.github.com>
2025-12-25 15:22:36 +0400

    добавил новые эндпоинты в main.py

--- frontend/public_landing_backup/main.tsx
+++
@@ -1,7 +0,0 @@
-// /frontend/public_landing/main.tsx
-
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./public_landing.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
