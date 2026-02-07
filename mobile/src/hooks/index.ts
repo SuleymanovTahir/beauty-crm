8bf9e7a11db048829976faad9f440025ed5ec9a5
SuleymanovTahir <90999791+SuleymanovTahir@users.noreply.github.com>
2026-02-07 21:20:52 +0400

    добавил новые эндпоинты в main.py

--- mobile/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export { useNotifications, scheduleLocalNotification, cancelAllNotifications, getBadgeCount, setBadgeCount } from './useNotifications';
-export { useOffline, getNetworkStatus } from './useOffline';
-export { useServices, useEmployees } from './useServices';
-export { useClientBookings, useEmployeeBookings } from './useBookings';
-export { useWebSocket, useNotificationsWebSocket } from './useWebSocket';
-export { useGoogleAuth } from './useGoogleAuth';
